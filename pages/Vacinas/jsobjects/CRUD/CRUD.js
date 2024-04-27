f8af06f9d0e3b02daebdae2cc6d2d870c95b47f6
felipe <felipefo@gmail.com>
2024-04-27 13:50:07 +0000

    .

--- pages/Vacinas/jsobjects/CRUD/CRUD.js
+++
@@ -1,9 +0,0 @@
-export default {
-	async create() {
-		let data = await Create.run().catch(error => {
-			let message = Create.data;
-			service.setUIError(message);//vai salvar o erro em um widget com o nome errorWidget
-		});
-		return data;
-	}
-}
